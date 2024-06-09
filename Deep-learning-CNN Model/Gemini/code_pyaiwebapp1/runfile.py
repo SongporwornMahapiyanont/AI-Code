63541f030a400e54a8e1cfc0d3bb6865ae437030
Songporworn Mahapiyanont <songporwornmahapiyanont@gmail.com>
2024-06-09 15:19:24 +0700

    Update runfile.py

--- Deep-learning-CNN Model/Gemini/code_pyaiwebapp1/runfile.py
+++ Deep-learning-CNN Model/Gemini/code_pyaiwebapp1/runfile.py
@@ -1,9 +1,5 @@
 import subprocess 
 
-file = "PYAIWA025.py"
+file = "D:\AI\AI-Code\Deep-learning-CNN Model\Gemini\code_pyaiwebapp1\PYAIWA011.py"
 runrun = 'streamlit run '+file #streamlit run file.py
 subprocess.Popen(runrun)
-
-
-
-
